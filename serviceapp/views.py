75974a70ea6f1a9f25f03da033fa980b688a3816
kisxo <udaysubba2004@gmail.com>
2024-10-27 00:56:02 +0530

    add index route
    index route is added and url files are configured

--- serviceapp/views.py
+++ serviceapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 
-# Create your views here.
+def index(request):
+    return HttpResponse('ho')
